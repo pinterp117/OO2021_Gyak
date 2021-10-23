99eadd96827dc107091d6115600a47ac62bc3ae7
pinterp117 <pinterp117@gmail.com>
2021-10-23 14:53:18 +0200

    Const referenciak

--- WarriorRPG/main.cpp
+++ WarriorRPG/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-void printStatus(string& name1, int hp1, int dmg1, string& name2, int hp2, int dmg2) {
+void printStatus(const string& name1, const int hp1, const int dmg1, const string& name2, const int hp2, const int dmg2) {
     cout << name1 << " (HP: " << hp1 << " DMG: " << dmg1 <<")  ---  " << name2 <<" (HP: " << hp2 << " DMG: " << dmg2 << ")" << endl;
 }
 
-void attack(string& attacker_name, int& attacker_hp, int& attacker_dmg, string& defender_name, int& defender_hp, int& defender_dmg) {
+void attack(const string& attacker_name, const int attacker_hp, const int attacker_dmg, string& defender_name, int& defender_hp, int& defender_dmg) {
     printStatus(attacker_name, attacker_hp, attacker_dmg, defender_name, defender_hp, defender_dmg);
     defender_hp-=attacker_dmg;
-    if (defender_hp<0) defender_hp=0;
+    if (defender_hp<=0) {
+        defender_name += " DEAD";
+        defender_hp = 0;
+        defender_dmg = 0;
+    }
 }
 
 int main() {
