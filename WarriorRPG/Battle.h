5bbf5f694657542662e8d9a22ab1b3c0fc3c6d4b
pinterp117 <pinterp117@gmail.com>
2021-10-23 16:34:04 +0200

    Headerek es tobb fajl

---
+++ WarriorRPG/Battle.h
@@ -0,0 +1,9 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+
+#include "Warrior.h"
+
+void printStatus(const Warrior& warrior1, const Warrior& warrior2);
+void fightTilDeath(Warrior& warrior1, Warrior& warrior2);
+
+#endif
---
+++ WarriorRPG/Warrior.h
@@ -0,0 +1,17 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+
+#include <string>
+
+struct Warrior {
+    std::string name;
+    int health_points;
+    int damage;
+};
+
+Warrior readWarrior();
+void printWarrior(const Warrior& warrior);
+void attack(const Warrior& attacker, Warrior& defender);
+bool isAlive(const Warrior& warrior);
+
+#endif
